54045f22dcc651d3a7bbe934e46a22c6284127b3
Naveen <pandurebelsachin18@gmail.com>
2021-12-26 11:25:29 +0530

    added Hasan and Trip

--- DP-1/Hasan-and-trip.cpp
+++ DP-1/Hasan-and-trip.cpp
@@ -1,43 +1,59 @@
-#include<bits/stdc++.h>
-#include<iostream>
-#define fin(i, s, n) for(int i = s; i<n; i++)
-#define fde(i, n) for(int i = n; i>=0; i--)
+#include <bits/stdc++.h>
+#include <iostream>
+#define fin(i, s, n) for (int i = s; i < n; i++)
+#define fde(i, n) for (int i = n; i >= 0; i--)
 #define ll long long
 #define mod 1000000007
 using namespace std;
-double maxHappines(int x[], int y[], int k[], int n){
-    double ans;
-    fin(i, 0, n){
-        cout << x[i] << ' ';
+// set precesion
+// cout << fixed << setprecision(6) << ans  <<endl;
+double calcDist(double x[], double y[], int i, int j)
+{
+  double ans;
+  ans = sqrt(pow(x[i] - x[j], 2) + pow(y[i] - y[j], 2) * 1.0);
+  return ans;
+}
+void maxHappines(double x[], double y[], double k[], int n)
+{
+  double dp[n];
+  fin(i, 0, n)
+  {
+    dp[i] = INT_MIN;
+  }
+  dp[0] = k[0];
+  fin(i, 1, n)
+  {
+    fin(j, 0, i)
+    {
+      // distance betwen i, j
+      double dist = calcDist(x, y, i, j);
+      double smallAns = dp[j] + k[i] - dist;
+      // printf("(i=%d, j=%d)=>smallAns = %f %f %f %f\n", i, j, dp[j], k[i], dist, smallAns);
+      if (smallAns > dp[i])
+      {
+        dp[i] = smallAns;
+      }
     }
-    cout << endl;
-    fin(i, 0, n){
-        cout << y[i] << ' ';
-    }
-    cout << endl;
-    fin(i, 0, n){
-        cout << k[i] << ' ';
-    }
-    cout << endl;
-    cout << endl;
-    return ans;
-} 
+  }
+  cout << dp[n - 1] << endl;
+}
 int main()
 {
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    int t;
-    cin >> t;
-    while(t--){
-        int n;
-        cin >> n;
-        int x[n], y[n], k[n];
-        fin(i, 0, n){
-            cin >> x[i] >> y[i] >> k[i];
-        }
-        std::cout << std::setprecision(6) << std::fixed;
-        cout << maxHappines( x,  y,  k,  n) << endl; 
-        cout << endl;
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int n;
+    cin >> n;
+    double x[n], y[n], k[n];
+    fin(i, 0, n)
+    {
+      cin >> x[i] >> y[i] >> k[i];
     }
-    return 0;
+    cout << fixed << setprecision(6);
+    maxHappines(x, y, k, n);
+  }
+  return 0;
 }
